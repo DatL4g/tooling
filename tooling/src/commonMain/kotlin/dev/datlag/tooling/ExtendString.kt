4e75207d5d9ce16e9710ff7c80fdaca60d3c437d
DatLag <jeff.retz08@gmail.com>
2024-01-05 00:58:38 +0100

    fixed build

--- tooling/src/commonMain/kotlin/dev/datlag/tooling/ExtendString.kt
+++ tooling/src/commonMain/kotlin/dev/datlag/tooling/ExtendString.kt
@@ -1,6 +1,6 @@
 package dev.datlag.tooling
 
 fun String.getDigitsOrNull(): String? {
-    val replaced = this.replace("\\D+".toRegex(), String())
+    val replaced = this.replace("\\D+".toRegex(), "")
     return replaced.ifBlank { null }
 }
