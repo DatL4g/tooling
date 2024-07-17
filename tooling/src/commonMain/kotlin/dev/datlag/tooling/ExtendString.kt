d9c206b15c7e77a183d4f3cbf52a9bad7d307624
DatLag <jeff.retz08@gmail.com>
2024-07-17 21:41:29 +0200

    added safe substring

--- tooling/src/commonMain/kotlin/dev/datlag/tooling/ExtendString.kt
+++ tooling/src/commonMain/kotlin/dev/datlag/tooling/ExtendString.kt
@@ -1,5 +1,8 @@
 package dev.datlag.tooling
 
+import kotlin.math.max
+import kotlin.math.min
+
 /**
  * Get all numbers of a given [String].
  * This does not include floating points.
@@ -9,4 +12,20 @@ package dev.datlag.tooling
 fun String.getDigitsOrNull(): String? {
     val replaced = this.replace("\\D+".toRegex(), "")
     return replaced.ifBlank { null }
+}
+
+fun CharSequence.safeSubSequence(from: Int, to: Int): CharSequence {
+    if (this.isEmpty() || from > lastIndex) {
+        return ""
+    }
+
+    val safeFrom = max(min(from, lastIndex), 0)
+    return this.subSequence(
+        safeFrom,
+        max(safeFrom, min(to, length))
+    )
+}
+
+fun String.safeSubString(from: Int, to: Int): String {
+    return safeSubSequence(from, to).toString()
 }
